794316f018ba03ac271f0f6916a2f24c7f7e797f
Igor Gunin <gunin-igor75@yandex.ru>
2023-08-16 12:17:50 +0600

    first commit

---
+++ src/main/java/com/github/guninigor75/news_line/NewsLineApplication.java
@@ -0,0 +1,13 @@
+package com.github.guninigor75.news_line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/guninigor75/news_line/NewsLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guninigor75.news_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
